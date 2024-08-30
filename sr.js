8270b4e4bc0b3dd7406003068bc92750595af675
Shahab-Saeed <161280552+Shahab-Saeed@users.noreply.github.com>
2024-08-30 11:54:19 +0500

    Add files via upload

---
+++ sr.js
@@ -0,0 +1,34 @@
+// ASSIGNMENT 1
+
+// QUESTION 1
+
+// alert("Welcome To Our Website")
+
+
+//QUESTION 2
+
+// alert("Error! Please enter a valid password")
+
+
+//QUESTION 3
+
+// ("<p>"+'Welcome to JS Land \n Happy Coding'+"</p>");
+
+
+//QUESTION 4
+
+// alert('Welcome to JS Land');
+// alert("Happy Coding!\n \n Prevent this page from creating additional dialogs");
+
+
+//QUESTION 5
+
+// alert("Hello... I can run JS through my web browser's console");
+
+
+//QUESTION 6
+
+// alert("Hello... I can run JS through my web browser's console");
+
+// QUESTION 7
+// On index.html
